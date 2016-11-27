b86e634bfd03a572aea53a0ee64f3dc49043e28f
Muaz Kurt <muazkurt@gmail.com>
2016-11-27 22:51:46 +0300

    süsülü yazdırma kaldı

--- HW06_MUAZ_KURT_151044062_praser.c
+++ HW06_MUAZ_KURT_151044062_praser.c
@@ -1,28 +1,25 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#define posibleChar 10000
-#define maxMailcanbeReaden 20
-#define mssSubject 200
-#define mssBody 2000
+#define posibleChar	 4096	//Max readable chars from file.
+#define mssEmail	16			//MeSage String for Email.
+#define mssSubject 32		//MeSage String for Subject.
+#define mssBody 1024		//MeSage String for Body.
+#define mpSubject 15		//Maximum Printable Subject
+#define mpBody 50			//Maximum Printable Body
 
 /*
     I can only read posibleChar characters from a file.
     If the file finish before posibleChar,
     then my job ends.
 */
-char *readingFile(
-                  FILE* data,
-                  char *fullreadenfile)
+char *readingFile(FILE* data, char *fullreadenfile)
 {
     int counter;
     char q;
-    for(counter = 0;
-            fscanf(data, "%c", &q)!= EOF &&
-            counter < posibleChar - 1;
-                ++counter)
-        fullreadenfile[counter] = q;
-    fullreadenfile[counter] = 0;
+	for (counter = 0; fscanf(data, "%c", &q) != EOF && counter < posibleChar - 1; ++counter)
+		fullreadenfile[counter] = q;
+    fullreadenfile[counter] = '\0';
     return fullreadenfile;
 }
 
@@ -34,17 +31,19 @@ char *readingFile(
     and return it.
     If there is no keyword, then I return NULL for saying there will not be keyword.
 */
-char *whatsAfterThis(
-                     char *fullreadenfile,
-                     char *start)
+char *whatsAfterThis(char *fullreadenfile, char *start)
 {
     int counter;
-    for(counter = 0; counter < strlen(fullreadenfile); ++counter)
-    {
-        if(strncmp(&fullreadenfile[counter], start, strlen(start)) == 0)
-            return strcpy(fullreadenfile,
-                          &fullreadenfile[counter + strlen(start)]);
-    }
+	for (counter = 0; counter < strlen(fullreadenfile); ++counter)
+	{
+		if (strncmp(&fullreadenfile[counter], start, strlen(start)) == 0)
+		{
+			if (counter > 0)
+				return strcpy(fullreadenfile, &fullreadenfile[counter + strlen(start)]);
+			else if (counter == 0)
+				return strcpy(fullreadenfile, &fullreadenfile[counter + strlen(start)- 1]);
+		}
+	}
     return "Empty";
 }
 
@@ -56,22 +55,14 @@ char *whatsAfterThis(
     If i cant find the keyword in given string,
     Then i return Empty as there is no keyword.
 */
-//i think there must be a update.
-char *whatsBeforeThis(
-                      char *fullreadenfile,
-                      char *finish,
-					  char *whatsBefore)
+char *whatsBeforeThis(char *fullreadenfile, char *finish, char *whatsBefore)
 {
     int counter;
-    for(counter = 0; counter < strlen(fullreadenfile); ++counter)
-    {
-		if (strncmp(&fullreadenfile[counter],
-			finish,
-			strlen(finish)) == 0)
-			return strncpy(whatsBefore,
-				fullreadenfile,
-				counter);
-    }
+	for (counter = 0; counter < strlen(fullreadenfile); ++counter)
+	{
+		if (strncmp(&fullreadenfile[counter], finish, strlen(finish)) == 0)
+			return strncpy(whatsBefore, fullreadenfile, counter);
+	}
     return "Empty";
 }
 
@@ -81,56 +72,52 @@ char *whatsBeforeThis(
     I'm finding the string between first keyword and sec keyword.
     I'm searching with early defined functions "whatsAfterThis" and "whatsBeforeThis".
     I return the string between them.
-    If there will be some errors, then return value will be NULL.
+    If there will be some errors, then return value will be Empty.
 */
-char *whatsBetweenThese(
-                        char *fullreadenfile,
-                        char *start,
-                        char *finish,
-						char *placetoPut)
+char *whatsBetweenThese(char *fullreadenfile, char *start, char *finish, char *placetoPut)
 {
-    return strcpy(placetoPut,
-                  whatsBeforeThis(strcpy(placetoPut,
-                                          whatsAfterThis(fullreadenfile,
-                                                         start)),
-                                          finish,placetoPut));
+	whatsBeforeThis(fullreadenfile, finish, placetoPut);
+	whatsAfterThis(placetoPut, start);
+	whatsAfterThis(fullreadenfile, finish);
+	return placetoPut;
 }
 
-void parsingMail(FILE *openedmail,
-                 char subject[maxMailcanbeReaden][mssSubject],
-                 char body[maxMailcanbeReaden][mssBody])
+/*
+	I am the hearth of all early defined functions.
+	I am using all of them inside me.
+	My job is finding <email> , </email> tags to finding emails
+	Then I find <Subject>, </Subject tags to find the subject
+	After that I find <Body>, </Body> tags and find the body.
+	I need opened file pointer, two double dimentional arrays to put subject and body.
+*/
+void parsingMail(FILE *openedmail, char subject[mssEmail][mssSubject], char body[mssEmail][mssBody])
 {
-    int counter;
-    char theFile[posibleChar], mailweworkon[posibleChar];
-    readingFile(openedmail,theFile);
+	int counter;
+	char theFile[posibleChar] = "\0", mailweworkon[posibleChar] = "\0";
+	readingFile(openedmail, theFile);
 	for (counter = 0;
-					whatsBetweenThese(theFile,
-									"<email>",
-									"</email>",mailweworkon) != "Empty" &&
-									counter < maxMailcanbeReaden;
-									++counter)
-    {
-               whatsBetweenThese(mailweworkon,
-                                 "<Subject>",
-                                 "</Subject>",subject[counter]);
-               whatsBetweenThese(mailweworkon,
-                                 "<Body>",
-                                 "</Body>",body[counter]);
-    }
-    return;
+		whatsBetweenThese(theFile, "<email>", "</email>", mailweworkon) != "Empty" &&
+		counter < mssEmail;
+		++counter)
+		{
+			whatsBetweenThese(mailweworkon, "<Subject>", "</Subject>", subject[counter]);
+			whatsBetweenThese(mailweworkon, "<Body>", "</Body>", body[counter]);
+		}
+	return;
 }
+
+
 
 
 
 void main()
 {
     FILE *emails;
-    char subject[maxMailcanbeReaden][mssSubject],
-        body[maxMailcanbeReaden][mssBody];
+	char subject[mssEmail][mssSubject], body[mssEmail][mssBody];
     emails=fopen("emails.txt", "r");
-    parsingMail(emails, subject, body);
-    printf("%s\n\n\n", subject[0]);
-  //  printf("%s\n\n\n", body[0]);
+    parsingMail(emails, subject, body);	
+	printf(".......%s....\n\n", subject[0]);
+	printf(".......%s....\n", body[0]);
     fclose(emails);
     return;
 }
